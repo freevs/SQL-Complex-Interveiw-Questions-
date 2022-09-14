9a01b5116d1ed6fb00a26d571732d9ff5759ce45
Freeva Racheal <38135441+freevs@users.noreply.github.com>
2022-09-14 19:19:28 +0530

    Add files via upload

---
+++ English, German, French , Spanish Speakers/3. English, German, French, Spanish Speakers.sql
@@ -0,0 +1,25 @@
+create table company_users 
+(
+company_id int,
+user_id int,
+language varchar(20)
+);
+
+insert into company_users values (1,1,'English')
+,(1,1,'German')
+,(1,2,'English')
+,(1,3,'German')
+,(1,3,'English')
+,(1,4,'English')
+,(2,5,'English')
+,(2,5,'German')
+,(2,5,'Spanish')
+,(2,6,'German')
+,(2,6,'Spanish')
+,(2,7,'English');
+
+
+select company_id from company_users
+where language in ('English', 'German')
+group by company_id
+having count(distinct user_id)>=2
